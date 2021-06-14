092f024aa3637235de3190e1bf0a52aa4cee615c
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-14 16:15:33 +0200

    Support passing pond options

--- projects/ng-pond/src/lib/ng-pond.service.spec.ts
+++ projects/ng-pond/src/lib/ng-pond.service.spec.ts
@@ -1,3 +1,4 @@
+import { FishErrorContext, FishId } from '@actyx/pond';
 import { TestBed } from '@angular/core/testing';
 
 import { ActyxPondService } from './ng-pond.service';
@@ -12,5 +13,39 @@ describe('ActyxPondService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+});
+
+describe('ActyxPondServiceOptions', () => {
+  let service: ActyxPondService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: 'pondOptions',
+          useValue: {
+            defaultSnapshotThreshold: 42,
+            fishErrorReporter: (
+              err: unknown,
+              fishId: FishId,
+              detail: FishErrorContext
+            ) => console.error(err, fishId, detail),
+          },
+        },
+        ActyxPondService,
+      ],
+    });
+    service = TestBed.inject(ActyxPondService);
+  });
+
+  it('should be passed to pond', async () => {
+    expect(
+      await service
+        .getPond()
+        //@ts-expect-error
+        .then((pond) => pond.opts)
+        .then((opts) => opts.defaultSnapshotThreshold)
+    ).toBe(42);
   });
 });
